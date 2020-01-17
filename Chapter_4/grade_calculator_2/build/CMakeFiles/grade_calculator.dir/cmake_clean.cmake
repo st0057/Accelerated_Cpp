7f2e2aa1987d2d889207d21c0d5516b661074878
Shaun Steele <sasst55@hotmail.com>
2020-01-17 21:36:43 +0000

    Updated grade calculator folder for problem 4-6

---
+++ Chapter_4/grade_calculator_2/build/CMakeFiles/grade_calculator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grade_calculator.dir/grade_calc_main.cpp.o"
+  "CMakeFiles/grade_calculator.dir/grade.cpp.o"
+  "CMakeFiles/grade_calculator.dir/median.cpp.o"
+  "CMakeFiles/grade_calculator.dir/Student_info.cpp.o"
+  "grade_calculator.pdb"
+  "grade_calculator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grade_calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
