5719a6a06b2e79e602a0d7197b38ee8c1db36b07
st0057 <sasst55@hotmail.com>
2020-01-23 16:06:49 +0000

    width function initial commit; untested.

---
+++ Chapter_5/5.0/Frame_picture/width.cpp
@@ -0,0 +1,11 @@
+#include "width.h"
+
+using std::string;    using std::vector;
+
+string::size_type width(const vector<string>& v)
+{
+  string::size_type maxlen = 0;
+  for(vector<string>::size_type i = 0; i != v.size(); ++i)
+    maxlen = max(maxlen, v[i].size());
+  return maxlen;
+}
