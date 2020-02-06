853e2f8100ab56b7c7597091b0474ffef9f980a4
st0057 <sasst55@hotmail.com>
2020-02-06 23:31:50 +0000

    Initial commit of files.

---
+++ Chapter_5/5.1/permuted_index.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using std::string;      using std::vector;
+
+int main() {
+  vector<string> my_vec = ["The quick brown fox", "jumped over the fence"];
+
+  
+}
