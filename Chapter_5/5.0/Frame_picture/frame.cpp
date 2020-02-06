13104fb616da1c3c50b0a1feee565379a114ee08
st0057 <sasst55@hotmail.com>
2020-02-06 22:58:49 +0000

    Add and tested hcat function.

--- Chapter_5/5.0/Frame_picture/frame.cpp
+++ Chapter_5/5.0/Frame_picture/frame.cpp
@@ -5,6 +5,40 @@
 using std::string;    using std::vector;
 using std::cout;      using std::endl;
 
+vector<string> hcat(const vector<string>& left, const vector<string>& right)
+{
+  vector<string> ret;
+
+  // add 1 to leave a space between pictures
+  string::size_type width1 = width(left) + 1;
+
+  // indices to look at elements from left and right respectively
+  vector<string>::size_type i = 0, j = 0;
+
+  // continue until we've seen all rows from both pictures
+  while (i != left.size() || j != right.size()) {
+    // construct new string to hold characters from both pics
+    string s;
+
+    // copy a row from the left-hand side, if there is one
+    if (i != left.size())
+      s = left[i++];
+
+    // pad to full width
+    s += string(width1 - s.size(), ' ');
+
+    // copy a row from the right hand side, if there is one
+    if (j != right.size())
+      s += right[j++];
+
+    // add s to the picture we're creating
+    ret.push_back(s);
+  }
+
+  return ret;
+
+}
+
 vector<string> vcat(const vector<string>& top, const vector<string>& bottom)
 {
   // copy the top picture
@@ -38,19 +72,25 @@ vector<string> frame(const vector<string>& v)
 
 // Used for testing this function
 int main() {
-  vector<string> abc, abc1, abc_cat, abc_cat_framed;
+  vector<string> abc, abc1, abc_vcat, abc_hcat, abc_vcat_framed, abc_hcat_framed;
 
   abc.push_back("abc");
   abc.push_back("bcd");
   abc1.push_back("cdef");
   abc1.push_back("cdef");
 
-  abc_cat = vcat(abc, abc1);
+  abc_vcat = vcat(abc, abc1);
+  abc_hcat = hcat(abc, abc1);
 
-  abc_cat_framed = frame(abc_cat);
+  abc_vcat_framed = frame(abc_vcat);
+  abc_hcat_framed = frame(abc_hcat);
 
-  for (vector<string>::size_type i = 0; i != abc_cat_framed.size(); ++i) {
-    cout << abc_cat_framed[i] << endl;
+  for (vector<string>::size_type i = 0; i != abc_vcat_framed.size(); ++i) {
+    cout << abc_vcat_framed[i] << endl;
+  }
+
+  for (vector<string>::size_type i = 0; i != abc_hcat_framed.size(); ++i) {
+    cout << abc_hcat_framed[i] << endl;
   }
 
   return 0;
--- Chapter_5/5.0/Frame_picture/frame.h
+++ Chapter_5/5.0/Frame_picture/frame.h
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 
+std::vector<std::string> hcat(const std::vector<std::string>&, const std::vector<std::string>&);
+
 std::vector<std::string> vcat(const std::vector<std::string>&, const std::vector<std::string>&);
 
 std::vector<std::string> frame(const std::vector<std::string>&);
