dad41df2fac4ffe0f1e9307e5ade44210b7c4dc6
st0057 <sasst55@hotmail.com>
2020-03-12 23:43:45 +0000

    Trying to extend program to have vector with no a or d and then find largest of that vector.

--- Chapter_5/5.11/find_ascender_descender.cpp
+++ Chapter_5/5.11/find_ascender_descender.cpp
@@ -5,6 +5,7 @@
 using std::vector;      using std::string;
 using std::endl;        using std::cout;
 using std::cin;         using std::istream;
+using std::sort; 
 
 vector<string> ascenders{"b","d","f","h","k","l","t"};
 vector<string> descenders{"g","j","p","q","y"};
@@ -25,7 +26,7 @@ istream& read(istream& in, vector<string>& vec)
   return in;
 }
 
-vector<string> find_ascenders(vector<string>& dictionary)
+vector<string> find_ascenders(vector<string>& dictionary, vector<string>& non_a_words)
 {
   vector<string> ascender_words;
 
@@ -38,32 +39,47 @@ vector<string> find_ascenders(vector<string>& dictionary)
     }
   }
 
+  non_a_words = find_unique_vector_items(dictionary, ascender_words);
+
   return ascender_words;
 }
 
-vector<string> find_descenders(vector<string>& dictionary)
+vector<string> find_descenders(vector<string>& dictionary, vector<string>& non_d_words)
 {
   vector<string> descender_words;
 
-for(vector<string>::iterator i = dictionary.begin(); i != dictionary.end(); ++i)
-  {
-    for(vector<string>::iterator j = descenders.begin(); j != descenders.end(); ++j)
+  for(vector<string>::iterator i = dictionary.begin(); i != dictionary.end(); ++i)
     {
-      if (i->find(*j) != string::npos)
-        descender_words.push_back(*i);
+      for(vector<string>::iterator j = descenders.begin(); j != descenders.end(); ++j)
+      {
+        if (i->find(*j) != string::npos)
+          descender_words.push_back(*i);
+      }
     }
-  }
+
+    non_d_words = find_unique_vector_items(dictionary, descender_words);
 
   return descender_words;
 }
 
+vector<string> find_unique_vector_items(vector<string>& v1, vector<string>& v2)
+{
+  sort(v1.begin(), v1.end());
+  sort(v2.begin(), v2.end());
+  vector<string> v3;
+  std::set_difference(v1.begin(), v1.end(), v2.begin(), v2.end(), std::back_inserter(v3));
+
+  return v3;
+}
+
 int main() {
-  vector<string> dictionary, ascender_words, descender_words;
+  vector<string> dictionary, ascender_words, descender_words, non_ascender_words, non_descender_words, neither;
 
   read(cin, dictionary);
 
-  ascender_words = find_ascenders(dictionary);
-  descender_words = find_descenders(dictionary);
+  ascender_words = find_ascenders(dictionary, non_ascender_words);
+  descender_words = find_descenders(dictionary, non_descender_words);
+  neither = find_unique_vector_items(non_ascender_words, non_descender_words);
 
   cout << "-----Ascender Words-----" << endl;
   for (vector<string>::iterator i = ascender_words.begin(); i != ascender_words.end(); ++i)
@@ -73,6 +89,12 @@ int main() {
 
   cout << "-----Descender Words-----" << endl;
   for (vector<string>::iterator i = descender_words.begin(); i != descender_words.end(); ++i)
+  {
+    cout << *i  << endl;
+  }
+
+  cout << "-----Words With Neither-----" << endl;
+  for (vector<string>::iterator i = neither.begin(); i != neither.end(); ++i)
   {
     cout << *i  << endl;
   }
--- Chapter_5/5.11/find_ascender_descender.h
+++ Chapter_5/5.11/find_ascender_descender.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> find_ascenders(std::vector<std::string>&);
-std::vector<std::string> find_descenders(std::vector<std::string>&);
+std::vector<std::string> find_ascenders(std::vector<std::string>&, std::vector<std::string>&);
+std::vector<std::string> find_descenders(std::vector<std::string>&, std::vector<std::string>&);
+std::vector<std::string> find_unique_vector_items(std::vector<std::string>&, std::vector<std::string>&);
