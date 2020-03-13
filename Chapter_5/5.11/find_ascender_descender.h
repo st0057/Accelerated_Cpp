d3552fa286188be24fc256debb6c4f271357da08
st0057 <sasst55@hotmail.com>
2020-03-13 18:18:12 +0000

    Addressed issue with multiple a and d words showing up multiple times.

--- Chapter_5/5.11/find_ascender_descender.h
+++ Chapter_5/5.11/find_ascender_descender.h
@@ -4,3 +4,4 @@
 std::vector<std::string> find_ascenders(std::vector<std::string>&, std::vector<std::string>&);
 std::vector<std::string> find_descenders(std::vector<std::string>&, std::vector<std::string>&);
 std::vector<std::string> find_unique_vector_items(std::vector<std::string>&, std::vector<std::string>&);
+std::vector<std::string> find_common_vector_items(std::vector<std::string>&, std::vector<std::string>&);
