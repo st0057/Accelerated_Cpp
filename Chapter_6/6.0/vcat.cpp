372784ddb8bf0626b12318fa500807b4e089ed84
st0057 <sasst55@hotmail.com>
2020-03-18 15:32:57 +0000

    Changed vcat to use copy instead of insert

---
+++ Chapter_6/6.0/vcat.cpp
@@ -0,0 +1,36 @@
+#include <iterator>
+//#include <iostream>
+#include "vcat.h"
+
+using std::vector;        using std::string;
+using std::back_inserter; using std::copy;
+using std::cout;          using std::endl;
+
+vector<string> vcat(const vector<string>& top, const vector<string>& bottom)
+{
+  // copy the top picture
+  vector<string> ret = top;
+
+  // copy entire bottom picture
+  copy(bottom.begin(), bottom.end(), back_inserter(ret));
+
+  return ret;
+}
+
+// Test the code
+// int main () {
+//   vector<string> abc, abc1, abc_vcat;
+
+//   abc.push_back("abc");
+//   abc.push_back("bcd");
+//   abc1.push_back("cdef");
+//   abc1.push_back("cdef");
+
+//   abc_vcat = vcat(abc, abc1);
+
+//   for (vector<string>::size_type i = 0; i != abc_vcat.size(); ++i) {
+//     cout << abc_vcat[i] << endl;
+//   }
+
+//   return 0;
+// }
---
+++ Chapter_6/6.0/vcat.h
@@ -0,0 +1,4 @@
+#include <string>
+#include <vector>
+
+std::vector<std::string> vcat(const std::vector<std::string>&, const std::vector<std::string>&);
