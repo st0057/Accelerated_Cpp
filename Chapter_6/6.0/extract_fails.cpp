b16bf4a6067998302fbca98a08059c8b9c2ad573
st0057 <sasst55@hotmail.com>
2020-05-04 22:44:05 +0000

    Added 1 pass algorithmic soln; passed test

--- Chapter_6/6.0/extract_fails.cpp
+++ Chapter_6/6.0/extract_fails.cpp
@@ -1,5 +1,6 @@
 #include "extract_fails.h"
 #include "grade.h"
+#include <algorithm>
 
 using std::vector;
 
@@ -8,6 +9,15 @@ vector<Student_info> extract_fails_1(vector<Student_info>& students)
   vector<Student_info> fail;
   remove_copy_if(students.begin(), students.end(), back_inserter(fail), pgrade);
   students.erase(remove_if(students.begin(), students.end(), fgrade), students.end());
+
+  return fail;
+}
+
+vector<Student_info> extract_fails_2(vector<Student_info>& students) 
+{
+  vector<Student_info>::iterator iter = stable_partition(students.begin(), students.end(), pgrade);
+  vector<Student_info> fail(iter, students.end());
+  students.erase(iter,students.end());
 
   return fail;
 }
--- Chapter_6/6.0/extract_fails.h
+++ Chapter_6/6.0/extract_fails.h
@@ -6,6 +6,7 @@
 #include "Student_info.h"
 
 std::vector<Student_info> extract_fails_1(std::vector<Student_info>&);
+std::vector<Student_info> extract_fails_2(std::vector<Student_info>&);
 bool fgrade(const Student_info&); 
 bool pgrade(const Student_info&);
 
