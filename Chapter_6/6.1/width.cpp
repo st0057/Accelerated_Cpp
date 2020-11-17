312c0a40b98e99ea59141124bc89361702a2d2b1
st0057 <sasst55@hotmail.com>
2020-11-17 22:02:11 +0000

    Problem 6.1 finished

---
+++ Chapter_6/6.1/width.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "width.h"
+
+using std::string;    using std::vector;
+using std::max;
+
+string::size_type width(const vector<string>& v)
+{
+  string::size_type maxlen = 0;
+  for(vector<string>::size_type i = 0; i != v.size(); ++i)
+    maxlen = max(maxlen, v[i].size());
+  return maxlen;
+}
+
+// int main() {
+//   vector<string> abc;
+//   abc.push_back("abc");
+//   abc.push_back("abcde");
+
+//   string::size_type max = width(abc);
+//   std::cout << max << std::endl;
+
+//   return 0;
+// }
